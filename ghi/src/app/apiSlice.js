f60a74b083e218071761249457fea511b8886805
Brad <bradcochi@gmail.com>
2023-10-26 16:17:27 -0600

    starting front end deployment

--- ghi/src/app/apiSlice.js
+++ ghi/src/app/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const linktreeApi = createApi({
   reducerPath: "linktreeApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: process.env.REACT_APP_API_HOST,
     credentials: "include",
   }),
   endpoints: (builder) => ({
