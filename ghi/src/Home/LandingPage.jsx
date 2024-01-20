50a942be613199d6133e144ec732b92479043913
Brad <bradcochi@gmail.com>
2024-01-20 15:42:39 -0700

    changed LandingPage text color styling and sizing

--- ghi/src/Home/LandingPage.jsx
+++ ghi/src/Home/LandingPage.jsx
@@ -1,9 +1,15 @@
 const LandingPage = ({ account }) => {
   return (
     <div className="bg-[rgb(37,79,26)] pt-[calc(var(--header-padding)*2+var(--header-height))] px-6 pb-36">
-      <h1>Welcome to Linky!</h1>
+      <h1 className="text-[#D2E823] font-[820] text-4xl">
+        Everything you are. In one, simple link in bio.
+      </h1>
 
-      <p>A link sharing platform</p>
+      <p className="text-[#D2E823]">
+        Join 40M+ people using Linktree for their link in bio. One link to help
+        you share everything you create, curate and sell from your Instagram,
+        TikTok, Twitter, YouTube and other social media profiles.
+      </p>
     </div>
   );
 };
