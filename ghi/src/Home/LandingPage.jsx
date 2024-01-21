1e2acfd8fd4e1f2366b6c742abc626728e9d3562
Brad <bradcochi@gmail.com>
2024-01-20 18:22:44 -0700

    added IPhoneGraphic component to main page and edited icons

--- ghi/src/Home/LandingPage.jsx
+++ ghi/src/Home/LandingPage.jsx
@@ -3,21 +3,19 @@ import IPhoneGraphic from "../components/IPhoneGraphic";
 const LandingPage = ({ account }) => {
   return (
     <div className="bg-[rgb(37,79,26)] pt-[calc(var(--header-padding)*2+var(--header-height))] px-6 pb-36">
-      <div className="flex">
+      <div className="flex gap-24 items-center">
         <div>
-          <h1 className="text-[#D2E823] font-[820] text-[clamp(32px,8.5vmin,88px)] tracking-tight text-left">
+          <h1 className="text-[#D2E823] font-[820] text-[clamp(32px,8.5vmin,88px)] tracking-tight text-left leading-[1.05]">
             Everything you are. In one, simple link in bio.
           </h1>
 
-          <p className="text-[#D2E823] mt-[8px]">
+          <p className="text-[#D2E823] mt-[8px] text-[clamp(16px,2vmin,20px)]">
             Join 40M+ people using Linktree for their link in bio. One link to
             help you share everything you create, curate and sell from your
             Instagram, TikTok, Twitter, YouTube and other social media profiles.
           </p>
         </div>
-        <div>
-          <IPhoneGraphic />
-        </div>
+        <IPhoneGraphic />
       </div>
     </div>
   );
--- ghi/src/components/IPhoneGraphic.jsx
+++ ghi/src/components/IPhoneGraphic.jsx
@@ -1,47 +1,66 @@
+import { useEffect } from "react";
+import {
+  MessageCircleHeart,
+  Instagram,
+  Facebook,
+  Linkedin,
+} from "lucide-react";
+
 export default function IPhoneGraphic() {
-  var body = document.querySelector("body");
+  useEffect(() => {
+    var viewport = document.querySelector(".viewportWrapper");
 
-  window.addEventListener("mousemove", (event) => {
-    const innerWidth = event.view.innerWidth;
-    const halfInnerWidth = innerWidth / 2;
-    const clientX = event.clientX;
-    const widthDifference = Math.abs(clientX - halfInnerWidth);
-    const widthDifferencePercentage = (widthDifference / halfInnerWidth) * 100;
-    let rotateY = (widthDifferencePercentage / 100) * 30;
-    if (clientX < halfInnerWidth) rotateY = rotateY * -1;
+    window.addEventListener("mousemove", (event) => {
+      const innerWidth = event.view.innerWidth;
+      const halfInnerWidth = innerWidth / 2;
+      const clientX = event.clientX;
+      const widthDifference = Math.abs(clientX - halfInnerWidth);
+      const widthDifferencePercentage =
+        (widthDifference / halfInnerWidth) * 100;
+      let rotateY = (widthDifferencePercentage / 100) * 30;
+      if (clientX < halfInnerWidth) rotateY = rotateY * -1;
 
-    const innerHeight = event.view.innerHeight;
-    const halfInnerHeight = innerHeight / 2;
-    const clientY = event.clientY;
-    const heightDifference = Math.abs(clientY - halfInnerHeight);
-    const heightDifferencePercentage =
-      (heightDifference / halfInnerWidth) * 100;
-    let rotateX = (heightDifferencePercentage / 100) * 30;
-    if (clientY > halfInnerHeight) rotateX = rotateX * -1;
+      const innerHeight = event.view.innerHeight;
+      const halfInnerHeight = innerHeight / 2;
+      const clientY = event.clientY;
+      const heightDifference = Math.abs(clientY - halfInnerHeight);
+      const heightDifferencePercentage =
+        (heightDifference / halfInnerWidth) * 100;
+      let rotateX = (heightDifferencePercentage / 100) * 30;
+      if (clientY > halfInnerHeight) rotateX = rotateX * -1;
 
-    body.style.setProperty("--rotateY", rotateY);
-    body.style.setProperty("--rotateX", rotateX);
-  });
+      viewport.style.setProperty("--rotateY", rotateY);
+      viewport.style.setProperty("--rotateX", rotateX);
+    });
+  }, []);
 
   return (
-    <div class="viewport">
-      <div class="avatar"></div>
+    <div className="viewportWrapper">
+      <div className="viewport">
+        <div className="avatar"></div>
 
-      <div class="links">
-        <button>Autumn Collection</button>
-        <button>Latest additions</button>
-        <button>Podcast</button>
+        <div className="links">
+          <button>Autumn Collection</button>
+          <button>Latest additions</button>
+          <button>Podcast</button>
+        </div>
+
+        <div className="widget"></div>
+
+        <div className="icons">
+          <div className="icon">
+            <Instagram />
+          </div>
+          <div className="icon">
+            <Facebook />
+          </div>
+          <div className="icon">
+            <Linkedin />
+          </div>
+        </div>
+
+        <div className="item"></div>
       </div>
-
-      <div class="widget"></div>
-
-      <div class="icons">
-        <div class="icon">P</div>
-        <div class="icon">S</div>
-        <div class="icon">I</div>
-      </div>
-
-      <div class="item"></div>
     </div>
   );
 }
